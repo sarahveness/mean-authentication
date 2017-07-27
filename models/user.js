5e816652d71ec979186bf93628f8aa8d87301459
Sarah Veness <sarahhveness@gmail.com>
2017-07-26 17:43:58 -0700

    user model added

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
+
+// User Schema
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+//takes in the model and the schema
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = function(id, cb) {
+  User.findById(id, cb);
+}
+
+module.exports.getUserByUsername = function(username, cb) {
+  const query = {username: username}
+  User.findOne(query, cb);
+}
+
+module.exports.addUser = function(newUser, cb){
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if(err) throw err;
+      newUser.password = hash;
+      newUser.save(cb);
+    })
+  });
+}
